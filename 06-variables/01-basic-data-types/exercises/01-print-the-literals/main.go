49710cdf7c46444dc4dd6cb04ff767bf2efb7c12
Beth Pritchard <bpritchard@scottlogic.com>
2024-07-01 14:15:40 +0100

    Ex 06-01-01 - print literals

--- 06-variables/01-basic-data-types/exercises/01-print-the-literals/main.go
+++ 06-variables/01-basic-data-types/exercises/01-print-the-literals/main.go
@@ -8,6 +8,8 @@
 
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Print the literals
 //
@@ -25,4 +27,9 @@ package main
 
 func main() {
 	// Use fmt.Println()
+	fmt.Println(12, 45, 63, 3)
+	fmt.Println(12.4, 4.2, 5.7)
+	fmt.Println(true, false)
+	fmt.Println("Beth")
+	fmt.Println("Bonjour")
 }
